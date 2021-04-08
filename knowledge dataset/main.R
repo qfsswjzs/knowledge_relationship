540a1d1f1876095a8a2172035d18c99149b728e8
qfsswjzs <52876991+qfsswjzs@users.noreply.github.com>
2021-04-08 22:43:27 +0800

    network and prediction vs baseline (#2)
    Co-authored-by: 吴洁 <wujie@wjmba.local>

--- knowledge dataset/main.R
+++ knowledge dataset/main.R
@@ -3,6 +3,7 @@
 # Time      : 2021/4/2
 
 
+library(e1071)
 library(MASS)
 
 setwd("/Users/wujie/Documents/Projects/Pycharm/graduation_project/knowledge dataset")
@@ -17,75 +18,116 @@ p = 83
 data = getData()
 train_data = data$train_data
 test_data = data$test_data
-sex_test = data$sex_test
 
+early_semester = 1:59
+last_semester = 60:83
+
+early_train_data = train_data[, early_semester]
+early_test_data = test_data[, early_semester]
+last_train_data = train_data[, last_semester]
+last_test_data = test_data[, last_semester]
+
+fun_type = c("clime", "glasso")
 
 get_theta = function (fun){
   if(fun == "clime") theta = clime_knowledge(train_data)
   if(fun == "glasso") theta = glasso_knowledge(train_data)
+  if(fun == "sample") theta = solve(var(train_data))
   return (theta)
 }
 
 
-precision_classification = function (){
-  for(i in 1:nrow(train_data)){
-    for(j in 1:ncol(train_data)){
-      train_data[i, j] = train_data[i, j] / sd(train_data[i,])
-    }
+best_mse_forecast = function (fun_type){
+  mu_1 = apply(early_train_data, 2, mean)
+  mu_2 = apply(last_train_data, 2, mean)
+  Sigma = solve(get_theta(fun_type))
+
+  Sigma_11 = Sigma[early_semester, early_semester]
+  Sigma_21 = Sigma[last_semester, early_semester]
+
+  predict_values = matrix(0, nrow(last_test_data), ncol(last_test_data))
+  for(i in 1:nrow(early_test_data)){
+    predict_values[i,] = mu_2 + Sigma_21 %*% solve(Sigma_11) %*% (early_test_data[i,] - mu_1)
   }
 
-  for(i in 1:nrow(test_data)){
-    for(j in 1:ncol(test_data)){
-      test_data[i, j] = test_data[i, j] / (sd(test_data[i,]))
-    }
-  }
+  RMSE = sum((predict_values - last_test_data) ^ 2) / (nrow(last_test_data) * ncol(last_test_data))
+  MAE = sum(abs(predict_values - last_test_data)) / (nrow(last_test_data) * ncol(last_test_data))
+  error = list(RMSE = RMSE, MAE = MAE)
 
-  mu_male = vector(mode = "numeric", p)
-  mu_female = vector(mode = "numeric", p)
-  male_pi = 0.5
-  female_pi =0.5
-
-  for(i in 1:1400){
-    if(i <= 700) mu_male = mu_male + train_data[i,]
-    else mu_female = mu_female + train_data[i,]
-  }
-
-  mu_male = mu_male / 700
-  mu_female = mu_female / 700
-
-  mu_list = list(mu_male = mu_male, mu_female = mu_female)
-  pi_list = list(male_pi = male_pi, female_pi = female_pi)
-
-  sex = vector(mode = "character", p)
-  for(i in 1:600){
-    sex[i] = LDAfunction(theta, mu_list, pi_list, test_data[i,])
-  }
-
-  TP = 0
-  FP = 0
-  TN = 0
-  FN = 0
-
-  for(i in 1:600){
-    if(i <= 300){
-      if(sex[i] == "male") TP = TP + 1
-      else FN = FN + 1
-    }else{
-      if(sex[i] == "female") TN = TN  + 1
-      else FP = FP + 1
-    }
-  }
-  ACC = (TP + TN) / (TP + TN + TN + FP)
-  specifity = TN / (TN + FP)
-  sensitivity = TP / (TP + FN)
-  MCC = (TP * TN - FP * FN) / sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
-  cat(ACC, specifity, sensitivity, MCC, "\n")
+  return (error)
 }
 
-fun_type = c("clime", "glasso")
+
+svr_forcast = function (){
+  sse_error = vector("numeric", ncol(last_test_data))
+  sum_mae = vector("numeric", ncol(last_test_data))
+
+  for(i in 1:ncol(last_test_data)){
+    tsm = tune.svm(x = early_train_data,
+                   y = last_train_data[,i],
+                   gamma = 10^(-5:-1),
+                   cost = 10^(1:3)
+                  )
+    gamma = tsm$best.model$gamma
+    cost = tsm$best.model$cost
+    svr.model = svm(x = early_train_data,
+                    y = last_train_data[,i],
+                    type = "eps-regression",
+                    kernel = "radial",
+                    gamma = gamma,
+                    cost = cost
+                    )
+    predict_values = predict(svr.model, early_test_data)
+    sum_mae[i] = sum(abs(predict_values - last_test_data[,i]))
+    sse_error[i] = sum((predict_values - last_test_data[,i]) ^ 2)
+  }
+
+  RMSE = sum(sse_error) / (nrow(last_test_data) * ncol(last_test_data))
+  MAE = sum(sum_mae) / (nrow(last_test_data) * ncol(last_test_data))
+  error = list(RMSE = RMSE, MAE = MAE)
+
+  return (error)
+}
+
+
+linear_forecast = function (){
+  sse_error = vector("numeric", ncol(last_test_data))
+  sum_mae = vector("numeric", ncol(last_test_data))
+
+  for(i in 1:ncol(last_test_data)){
+    y = last_train_data[, i]
+    m = lm(y ~., data = as.data.frame(early_train_data))
+    predict_values = predict(m, as.data.frame(early_test_data))
+    sse_error[i] = sum((predict_values - last_test_data[,i]) ^ 2)
+    sum_mae[i] = sum(abs(predict_values - last_test_data[,i]))
+  }
+
+  RMSE = sum(sse_error) / (nrow(last_test_data) * ncol(last_test_data))
+  MAE = sum(sum_mae) / (nrow(last_test_data) * ncol(last_test_data))
+
+  error = list(RMSE = RMSE, MAE = MAE)
+  return (error)
+}
+
+
+errors = matrix(0, 2, 4)
+row_name = c('RMSE', 'MAE')
+col_name = c('clime', 'glasso', 'SVR' ,'lm')
+dimnames(errors) = list(row_name, col_name)
+
 for(i in 1:2){
-  theta = get_theta(fun_type[i])
+  error = best_mse_forecast(fun_type[i])
+  errors[,i] = c(error$RMSE, error$MAE)
 }
+
+errors[, 3] = svr_forcast()
+errors[, 4] = linear_forecast()
+
+print(error)
+
+
+
+
 
 
 
